1cd4fa3133f4267fcfa722a2e5c8c16da3d05880
Sameer_yadav <159073326+Goku2099@users.noreply.github.com>
2026-05-24 15:25:47 +0530

    Add exploratory Spark + Ozone integration setup
    Signed-off-by: Sameer_yadav <159073326+Goku2099@users.noreply.github.com>

--- ozone_test.py
+++ ozone_test.py
@@ -1,3 +1,7 @@
+""" Needed manual port-forward because initial Spark Connect
+startup took longer than SDK readiness timeout due to
+Hadoop AWS dependency downloads."""
+
 from pyspark.sql import SparkSession
 
 spark = (
