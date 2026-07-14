c52a67003b470add84c2da18046bf6abb52504bb
Sameer_yadav <159073326+Goku2099@users.noreply.github.com>
2026-07-15 02:38:28 +0530

    e2e test-fix-1
    Signed-off-by: Sameer_yadav <159073326+Goku2099@users.noreply.github.com>

--- hack/e2e-setup-cluster.sh
+++ hack/e2e-setup-cluster.sh
@@ -275,8 +275,11 @@ metadata:
   namespace: $NAMESPACE
 rules:
   - apiGroups: [""]
-    resources: ["pods", "pods/log", "services", "configmaps", "persistentvolumeclaims"]
+    resources: ["pods", "services", "configmaps", "persistentvolumeclaims"]
     verbs: ["get", "list", "watch", "create", "delete", "deletecollection", "patch", "update"]
+  - apiGroups: [""]
+    resources: ["pods/log"]
+    verbs: ["get"]
 ---
 apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
